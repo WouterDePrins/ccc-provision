8ce9688b340308cae06a91db32bce35306b700b4
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 14:08:42 +0200

    add php-sample dir

--- web.sh
+++ web.sh
@@ -2,5 +2,6 @@ sudo apt update
 sudo apt install apache2 -y
 sudo apt install php libapache2-mod-php -y
 sudo systemctl restart apache2
-sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/
+sudo mkdir /var/www/html/php-sample/
+sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/php-sample/
 #sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
