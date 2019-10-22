0074169ff0bec60ad0653a2421adc0e567db9f1d
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 16:33:50 +0200

    write to index

--- web.sh
+++ web.sh
@@ -5,8 +5,8 @@ sudo systemctl restart apache2
 sudo mkdir /var/www/html/php-sample/
 sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/php-sample/
 sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
-sudo rm -f /var/www/html/index.html
+#sudo rm -f /var/www/html/index.html
 sudo rm -rf /var/www/html/php-sample/
-sudo echo "%CliqrTier_Database_IP%" > /var/www/html/ip.html
-sudo echo "%JOB_NAME%" > /var/www/html/job.html
+sudo echo "%CliqrTier_Database_IP%" > /var/www/html/index.html
+sudo echo "%JOB_NAME%" > /var/www/html/index.html
 #sudo echo "export test%JOB_NAME%" >> .bash_profile
