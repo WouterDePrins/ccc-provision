d2a39a029fe112620c109b0f7fd72f907d5832cb
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 13:37:35 +0200

    delete upgrade

--- web.sh
+++ web.sh
@@ -1,7 +1,6 @@
 sudo apt update
-sudo apt upgrade -y
-sudo apt install apache2
-sudo apt install php libapache2-mod-php
+sudo apt install apache2 -y
+sudo apt install php libapache2-mod-php -y
 sudo systemctl restart apache2
 git clone https://github.com/robinverstraelen/php-sample.git
 sudo rm /var/www/html/index.html
