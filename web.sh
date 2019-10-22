cb3cf6dde6759feb01cf90e7bc697d65055a0a1b
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 13:43:44 +0200

    test without move

--- web.sh
+++ web.sh
@@ -4,4 +4,4 @@ sudo apt install php libapache2-mod-php -y
 sudo systemctl restart apache2
 git clone https://github.com/robinverstraelen/php-sample.git
 sudo rm /var/www/html/index.html
-sudo mv php-sample/index.php /var/www/html/index.php
+#sudo mv php-sample/index.php /var/www/html/index.php
