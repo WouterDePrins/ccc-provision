f911caf5e1c9b0717e281f95b85be5c697e629d0
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 13:50:11 +0200

    add git target dir

--- web.sh
+++ web.sh
@@ -2,6 +2,5 @@ sudo apt update
 sudo apt install apache2 -y
 sudo apt install php libapache2-mod-php -y
 sudo systemctl restart apache2
-git clone https://github.com/robinverstraelen/php-sample.git
-sudo rm /var/www/html/index.html
-#sudo mv php-sample/index.php /var/www/html/index.php
+sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/
+sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
