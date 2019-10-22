113e2bd53db56f7d8af60aedfae7baea2951e74d
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-22 14:16:14 +0200

    add move

--- web.sh
+++ web.sh
@@ -4,4 +4,4 @@ sudo apt install php libapache2-mod-php -y
 sudo systemctl restart apache2
 sudo mkdir /var/www/html/php-sample/
 sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/php-sample/
-#sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
+sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
