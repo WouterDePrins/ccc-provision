691de1bc4a50e8a7968c2932826726120078c88f
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-23 15:01:46 +0200

    add sudo

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,11 @@ source /usr/local/cliqr/etc/userenv
 sudo yum update
 wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo rpm -ivh mysql80-community-release-el7-3.noarch.rpm
-sudo yum install mysql-server
+sudo yum -y install mysql-server
 sudo systemctl start mysqld
+sudo mkdir /var/mysql
 temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123!'; flush privileges;" > reset_pass.sql
-mysql -u root --password="$temp_password" --connect-expired-password < reset_pass.sql
+sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123!'; flush privileges;" > /var/mysql/reset_pass.sql
+sudo mysql -u root --password="$temp_password" --connect-expired-password < /var/mysql/reset_pass.sql
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh
