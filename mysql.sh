82625283933c6825c1ea9dba672c249862a582e0
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-23 15:08:35 +0200

    comment

--- mysql.sh
+++ mysql.sh
@@ -5,8 +5,8 @@ sudo rpm -ivh mysql80-community-release-el7-3.noarch.rpm
 sudo yum -y install mysql-server
 sudo systemctl start mysqld
 sudo mkdir /var/mysql
-temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
-sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123!'; flush privileges;" > /var/mysql/reset_pass.sql
-sudo mysql -u root --password="$temp_password" --connect-expired-password < /var/mysql/reset_pass.sql
+#temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
+#sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123!'; flush privileges;" > /var/mysql/reset_pass.sql
+#sudo mysql -u root --password="$temp_password" --connect-expired-password < /var/mysql/reset_pass.sql
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh
