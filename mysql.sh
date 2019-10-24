06ded62733015a23f10719ba9fd0a7a783ce36fa
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-10-24 11:46:50 +0200

    zonderpath

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,14 @@
 source /usr/local/cliqr/etc/userenv
-yum -y update
-yum -y install wget
+sudo yum -y update
+sudo yum -y install wget
 wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-rpm -ivh mysql80-community-release-el7-3.noarch.rpm
-yum -y install mysql-server
-systemctl start mysqld
+sudo rpm -ivh mysql80-community-release-el7-3.noarch.rpm
+sudo yum -y install mysql-server
+sudo systemctl start mysqld
 temp_password=$(sudo grep password /var/log/mysqld.log | awk '{print $NF}')
-mkdir -p /var/mysql/
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123&'; flush privileges;" > /var/mysql/reset_pass.sql
-mysql -u root --password="$temp_password" --connect-expired-password < /var/mysql/reset_pass.sql
+sudo mkdir -p /var/mysql/
+sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123&'; flush privileges;" | tee reset_pass.sql > /dev/null
+#sudo mysql -u root --password="$temp_password" --connect-expired-password < reset_pass.sql
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh
 #sudo bash <(curl -s https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh)
