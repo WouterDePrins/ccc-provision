493992feb0e1da08b517c78d8ba06ff7f13b432c
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-04 16:03:48 +0100

    add mysql install

--- web.sh
+++ web.sh
@@ -1,7 +1,7 @@
 source /usr/local/cliqr/etc/userenv
 sudo apt update
 sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php -y
+sudo apt install php libapache2-mod-php php-mysql -y
 sudo sed -i "s/;extension=mysqli/extension=mysqli/g" /etc/php/7.2/apache2/php.ini
 sudo systemctl restart apache2
 sudo mkdir /var/www/html/php-sample/
