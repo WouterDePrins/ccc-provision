201e0895249866056183652dad9711b7051da01b
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-04 13:25:44 +0100

    user

--- mysql.sh
+++ mysql.sh
@@ -8,11 +8,11 @@ sudo systemctl start mysqld
 temp_password=$(sudo grep password /var/log/mysqld.log | awk '{print $NF}')
 sudo mkdir -p /var/mysql/
 sudo echo $(sudo grep password /var/log/mysqld.log | awk '{print $NF}') | sudo tee /home/cliqruser/pass > /dev/null
-sudo echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('C1sco123&');" | sudo tee /home/cliqruser/reset_pass.sql > /dev/null
+sudo echo "SET PASSWORD FOR 'appuser'@'%' = PASSWORD('C1sco123&');" | sudo tee /home/cliqruser/changepass.sql > /dev/null
 sudo echo "GRANT ALL PRIVILEGES ON *.* TO 'appuser'@'%' IDENTIFIED BY 'C1sco123&';" | sudo tee /home/cliqruser/newuser.sql > /dev/null
 #sudo echo "flush privileges;" | sudo tee /home/cliqruser/reset_pass.sql > /dev/null
 mysql -u root --password="$temp_password" --connect-expired-password < /home/cliqruser/newuser.sql
-
+mysql -u root --password="$temp_password" --connect-expired-password < /home/cliqruser/changepass.sql
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh
 #sudo bash <(curl -s https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh)
