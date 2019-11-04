29ac55d428acde7a7f40dae13353a33bb68bb08b
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-04 13:00:57 +0100

    fix pwd

--- mysql.sh
+++ mysql.sh
@@ -8,9 +8,9 @@ sudo systemctl start mysqld
 temp_password=$(sudo grep password /var/log/mysqld.log | awk '{print $NF}')
 sudo mkdir -p /var/mysql/
 sudo echo $(sudo grep password /var/log/mysqld.log | awk '{print $NF}') | sudo tee /home/cliqruser/pass > /dev/null
-sudo echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'C1sco123&'; flush privileges;" | sudo tee /home/cliqruser/reset_pass.sql > /dev/null
+sudo echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('C1sco123&');" | sudo tee /home/cliqruser/reset_pass.sql > /dev/null
 #sudo echo "flush privileges;" | sudo tee /home/cliqruser/reset_pass.sql > /dev/null
-#mysql -u root --password="$temp_password" --connect-expired-password < /home/cliqruser/reset_pass.sql
+mysql -u root --password="$temp_password" --connect-expired-password < /home/cliqruser/reset_pass.sql
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh
 #sudo bash <(curl -s https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/mysql.sh)
