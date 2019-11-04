0c9087af67891fa00fd16618642332b3b89ee535
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-04 15:48:46 +0100

    add uncomment extension

--- web.sh
+++ web.sh
@@ -2,6 +2,7 @@ source /usr/local/cliqr/etc/userenv
 sudo apt update
 sudo apt install apache2 -y
 sudo apt install php libapache2-mod-php -y
+sudo sed -i "s/;extension=mysqli/extension=mysqli/g" /etc/php/7.2/apache2/php.ini
 sudo systemctl restart apache2
 sudo mkdir /var/www/html/php-sample/
 sudo git clone https://github.com/robinverstraelen/php-sample.git /var/www/html/php-sample/
@@ -9,10 +10,5 @@ sudo mv /var/www/html/php-sample/index.php /var/www/html/index.php
 sudo rm -f /var/www/html/index.html
 sudo rm -rf /var/www/html/php-sample/
 sudo sed -i s/%DB_TIER_IP%/$CliqrTier_Database_PUBLIC_IP/g /var/www/html/index.php
-#sudo bash -c 'echo %CliqrTier_Database_IP% >> /var/www/html/index.html'
-#sudo bash -c 'echo %JOB_NAME% >> /var/www/html/index.html'
-#echo %JOB_NAME% > /home/cliqruser/job.txt
-#echo %CliqrTier_Database_IP% > /home/cliqruser/ip.txt
-#sudo echo "export test%JOB_NAME%" >> .bash_profile
 
 #;https://raw.githubusercontent.com/robinverstraelen/ccc-provision/master/web.sh
