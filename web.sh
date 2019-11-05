2b2dcb48cbf7a5dba99a019634a7d364fe83cb28
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-05 10:51:51 +0100

    unauthenticated

--- web.sh
+++ web.sh
@@ -1,7 +1,7 @@
 source /usr/local/cliqr/etc/userenv
 sudo apt update
-sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install apache2 -y --allow-unauthenticated
+sudo apt install php libapache2-mod-php php-mysql -y --allow-unauthenticated
 sudo sed -i "s/;extension=mysqli/extension=mysqli/g" /etc/php/7.2/apache2/php.ini
 sudo systemctl restart apache2
 sudo mkdir /var/www/html/php-sample/
