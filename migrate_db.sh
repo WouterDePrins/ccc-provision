a2e58415e47297b19f1e2ef645eb0609ed362620
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-12 14:28:34 +0100

    fix dump line

--- migrate_db.sh
+++ migrate_db.sh
@@ -2,7 +2,7 @@ mkdir -p /home/cliqruser/dbmigrate
 git clone https://github.com/robinverstraelen/ccc-provision.git /home/cliqruser/dbmigrate
 echo "delete from employees.employees where emp_no>11000" > /home/cliqruser/delete.sql
 mysql -u root --password="" < /home/cliqruser/delete.sql
-mysqldump -u root --password="" employees > /home/cliqruser/dbmigrate/dump.sql  ca
+mysqldump -u root --password="" employees > /home/cliqruser/dbmigrate/dump.sql
 cd /home/cliqruser/dbmigrate/
 git config --global user.name "robinverstraelen"
 git remote set-url origin https://robinverstraelen:6KxsLYShiGxc@github.com/robinverstraelen/ccc-provision.git
