8d31d62f5883445299d814a5fb3da52e4ffdd8a1
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-12 14:46:18 +0100

    update creds

--- restore_db.sh
+++ restore_db.sh
@@ -1,3 +1,3 @@
 mkdir -p /home/cliqruser/dbmigrate
 git clone https://github.com/robinverstraelen/ccc-provision.git /home/cliqruser/dbmigrate
-mysql -u appuser --password="C1sco123&" < /home/cliqruser/dbmigrate/dump.sql
+mysql -u root --password="" < /home/cliqruser/dbmigrate/dump.sql
