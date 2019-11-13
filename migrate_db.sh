e5b184fa59e14fee7a8160b8366d91eaacdb071b
Robin Verstraelen <robin.verstraelen@gmail.com>
2019-11-13 11:00:52 +0100

    migrate

--- migrate_db.sh
+++ migrate_db.sh
@@ -2,7 +2,8 @@ mkdir -p /home/cliqruser/dbmigrate
 git clone https://github.com/robinverstraelen/ccc-provision.git /home/cliqruser/dbmigrate
 echo "delete from employees.employees where emp_no>11000" > /home/cliqruser/delete.sql
 mysql -u root --password="" < /home/cliqruser/delete.sql
-mysqldump -u root --password="" employees > /home/cliqruser/dbmigrate/dump.sql
+echo "use employees;" > /home/cliqruser/dbmigrate/dump.sql
+mysqldump -u root --password="" employees >> /home/cliqruser/dbmigrate/dump.sql
 cd /home/cliqruser/dbmigrate/
 git config --global user.name "robinverstraelen"
 git remote set-url origin https://robinverstraelen:6KxsLYShiGxc@github.com/robinverstraelen/ccc-provision.git
